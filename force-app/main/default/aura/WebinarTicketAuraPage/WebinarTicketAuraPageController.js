6746050f489b1a807c1aeee5861729136ee25b43
npz <nafa.create@gmail.com>
2025-01-28 08:41:17 +0700

    latest update

--- force-app/main/default/aura/WebinarTicketAuraPage/WebinarTicketAuraPageController.js
+++ force-app/main/default/aura/WebinarTicketAuraPage/WebinarTicketAuraPageController.js
@@ -2,11 +2,12 @@
  * @description       : 
  * @author            : npz
  * @group             : 
- * @last modified on  : 27-01-2025
+ * @last modified on  : 28-01-2025
  * @last modified by  : npz
 **/
 ({
     doInit: function (component, event, helper) {
+        component.set("v.isLoading", true)
         component.set("v.statusOptions", [
             { label: "All", value: "" },
             { label: "Pending", value: "Pending" },
@@ -14,13 +15,17 @@
             { label: "Cancelled", value: "Cancelled" }
         ]);
         component.set("v.columns", [
-            { label: "Name", fieldName: "Name", type: "text" },
+            { label: "Name", fieldName: "Name", type: "text", sortable: true  },
             { label: "Email", fieldName: "Email__c", type: "email" },
-            { label: "Phone", fieldName: "Phone__c", type: "text" },
-            { label: "Ticket Status", fieldName: "Ticket_Status__c", type: "text" }
+            { label: "Phone", fieldName: "Phone__c", type: "phone" },
+            { label: "Ticket Status", fieldName: "Ticket_Status__c", type: "text", sortable: true }
         ]);
 
-        helper.fetchTickets(component, ""); // Fetch all tickets initially
+        // simulasi isLoading untuk data yang banyak
+        setTimeout($A.getCallback(function () {
+            helper.fetchTickets(component, "");    
+            component.set("v.isLoading", false);
+        }), 2000);
     },
 
     handleStatusChange: function (component, event, helper) {
@@ -39,8 +44,21 @@
     },
 
     handleTicketCreated: function (component, event, helper) {
-        // Refresh the table after a ticket is created
+        
         helper.fetchTickets(component, "");
         component.set("v.isModalOpen", false);
+    },
+    handleSort: function (component, event, helper) {
+        const fieldName = event.getParam('fieldName'); 
+        const sortDirection = event.getParam('sortDirection');
+
+        
+        component.set('v.sortedBy', fieldName);
+        component.set('v.sortDirection', sortDirection);
+
+        
+        const tickets = component.get('v.tickets');
+        helper.sortData(tickets, fieldName, sortDirection);
+        component.set('v.tickets', tickets);
     }
 })
